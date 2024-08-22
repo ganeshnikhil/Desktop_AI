e59815a5b550d9e4fb780283c858ec85528bd7a2
ganesh nikhil <73976037+ganeshnikhil@users.noreply.github.com>
2024-08-22 18:56:23 +0530

    Update stop_words.py

--- src/DATA/stop_words.py
+++ src/DATA/stop_words.py
@@ -1,13 +1,4 @@
-# custom_stop_words = [
-#         'a', 'an', 'the', 'is', 'it', 'to', 'and', 'of', 'in', 'for', 'on', 'with', 
-#         'at', 'by', 'from', 'as', 'that', 'which', 'this', 'these', 'those', 'has', 
-#         'have', 'had', 'were', 'was', 'be', 'been', 'being', 'are', 'am', 'or', 
-#         'but', 'if', 'when', 'then', 'else', 'so', 'more', 'less', 'some', 'many', 
-#         'each', 'every', 'most', 'other', 'own', 'same', 'such', 'only', 'just', 
-#         'also', 'how', 'where', 'what', 'why', 'who', 'whom', 'whose', 'which','us',
-#         'the', 'a', 'are', 'your', 'in', 'with', 'to', 'and', 'as', 'for', 'on','me',
-#         'task', 'role','.','tell','me','about','given','describe','task',"what's"
-#     ]
+
 
 custom_stop_words = list(set([
     'am', 'on', 'with', 'role', 'to', 'that', 
