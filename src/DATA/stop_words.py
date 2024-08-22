4afb88550f3640ee83530fde87760838cb947a9c
ganesh nikhil <73976037+ganeshnikhil@users.noreply.github.com>
2024-08-22 18:55:49 +0530

    Add files via upload
    added coustom stop words that make the cosine similarity more accurate.

---
+++ src/DATA/stop_words.py
@@ -0,0 +1,30 @@
+# custom_stop_words = [
+#         'a', 'an', 'the', 'is', 'it', 'to', 'and', 'of', 'in', 'for', 'on', 'with', 
+#         'at', 'by', 'from', 'as', 'that', 'which', 'this', 'these', 'those', 'has', 
+#         'have', 'had', 'were', 'was', 'be', 'been', 'being', 'are', 'am', 'or', 
+#         'but', 'if', 'when', 'then', 'else', 'so', 'more', 'less', 'some', 'many', 
+#         'each', 'every', 'most', 'other', 'own', 'same', 'such', 'only', 'just', 
+#         'also', 'how', 'where', 'what', 'why', 'who', 'whom', 'whose', 'which','us',
+#         'the', 'a', 'are', 'your', 'in', 'with', 'to', 'and', 'as', 'for', 'on','me',
+#         'task', 'role','.','tell','me','about','given','describe','task',"what's"
+#     ]
+
+custom_stop_words = list(set([
+    'am', 'on', 'with', 'role', 'to', 'that', 
+    'a', 'which', 'us', 'the', 'when', 'by', 
+    'about', 'tell', 'whom', 'been', 'less', 
+    'these', 'are', 'as', 'such', 'being', 
+    'those', 'if', 'more', 'some', 'who', 
+    'task', 'describe', 'had', 'an', 'also', 
+    'where', 'why', 'of', 'in', 'at', 'every', 
+    'just', 'this', 'and', 'was', 'each', 'it', 
+    'only', 'is', 'same', 'be', 'or', 'else', 
+    'your', 'most', 'whose', 'what', '.','other', 
+    'own', 'given', 'were', 'so', 'have', 'but', 
+    'me', 'how', 'for', "what's", 'then', 'many', 
+    'from', 'has',',','today','report','brief','conditions',
+    'summary','action']))
+
+
+
+
